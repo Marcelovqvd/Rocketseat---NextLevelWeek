6fc696f6c2a2d0da50593d24fc18e4323a576493
Marcelo Vasques <marcelovqvd@gmail.com>
2020-08-05 14:52:03 -0300

    input component

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
@@ -9,6 +11,27 @@ function TeacherForm() {
                 title="Que incrível que você quer dar aulas."
                 description="O primeiro passo é preencher esse formulário de inscrição"
             />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="name" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" id="whatsapp" />
+                    </div>
+                </fieldset>
+            </main>
         </div>
     )
 }
